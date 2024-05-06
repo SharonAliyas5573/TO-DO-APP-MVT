0ef177c868c5470e0148b5f21981610fb229204b
SharonAliyas5573 <sharonaliyas25@gmail.com>
2024-05-06 19:05:51 +0530

    Update settings and database configuration
    Remove unnecessary build files and vercel.json

--- TO_DO_MVT/settings.py
+++ TO_DO_MVT/settings.py
@@ -27,7 +27,7 @@ DEBUG = config("DEBUG", default=False, cast=bool)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = []
 
 # Application definition
 
@@ -79,10 +79,10 @@ SESSION_COOKIE_SAMESITE = 'Lax'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    # "default": {
-    #     "ENGINE": "django.db.backends.sqlite3",
-    #     "NAME": BASE_DIR / "db.sqlite3",
-    # }
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
 }
 
 
